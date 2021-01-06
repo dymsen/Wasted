2ba66b1fd4734f846f3fbc252ac034b13230a8b9
git.dymsen@pm.me <git.dymsen@pm.me>
2021-01-06 17:23:26 +0100

    CHanging size (smaller) of the map and clean it

---
+++ 3_Bathroom_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="3_Bathroom_32x32" tilewidth="32" tileheight="32" tilecount="352" columns="16">
+ <image source="../TiledMaps/TileSets/3_Bathroom_32x32.png" width="512" height="704"/>
+</tileset>
